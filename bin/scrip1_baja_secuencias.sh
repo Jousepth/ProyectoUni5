0ca903a98c284cf10d4c9337b6565576bf94f406
Jousepth <jousepthgallardo@gmail.com>
2020-02-20 22:06:22 -0300

    primer script de la tarea unidad 4-5

---
+++ bin/scrip1_baja_secuencias.sh
@@ -0,0 +1,11 @@
+#Scrip Proyect Units 4-5
+#Jousepth Gallardo HIdalgo
+#20/02/2020
+
+#1) Download the data of the wolves in vcf to the data directory
+
+cd ../data/
+wget https://datadryad.org/stash/downloads/file_stream/6226
+
+#changing name
+mv 6226 wolf_2020_JG.vcf
