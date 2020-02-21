73cfd74c8d752927088153d7f0b238b1af7202f4
Jousepth <jousepthgallardo@gmail.com>
2020-02-20 22:49:26 -0300

    Script para trbajar con VCFtools

---
+++ bin/script1_baja_secuencias.sh
@@ -0,0 +1,11 @@
+#Scrip Proyect Units 4-5
+#Jousepth Gallardo HIdalgo
+#20/02/2020
+
+#1) Download the data of the wolves in vcf to the data directory
+
+cd ../data/
+curl https://datadryad.org/stash/downloads/file_stream/6226>wolf_2020_JG.vcf
+
+#changing name
+#mv 6226 wolf_2020_JG.vcf
---
+++ bin/script2_VCFtools.sh
@@ -0,0 +1,4 @@
+#VCFtools script with various functions
+#Jousepth Gallardo HIdalgo
+#20/02/2020
+
