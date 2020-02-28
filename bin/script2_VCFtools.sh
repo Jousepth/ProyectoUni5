4de9233f89947e68eefc64f7cacfae25e713ca27
Jousepth <jousepthgallardo@gmail.com>
2020-02-27 23:58:09 -0300

    script with vcftools functions

--- bin/script2_VCFtools.sh
+++ bin/script2_VCFtools.sh
@@ -1,4 +1,33 @@
-#VCFtools script with various functions
+#Script VCFtools with several typical functions
 #Jousepth Gallardo HIdalgo
 #20/02/2020
 
+#a) How many individuals and variants (SNPs) does the file have?
+vcftools --vcf ../data/wolf_2020_JG.vcf
+
+#b) Calculate the frequency of each allele for all individuals within the file and save the result in a file.
+vcftools --vcf ../data/wolf_2020_JG.vcf --freq --out ../data/frecuencias
+
+#c) How many sites in the archive do not have missing data?
+vcftools --vcf ../data/wolf_2020_JG.vcf --max-missing 1
+
+#d) Calculate the frequency of each allele for all individuals but only for sites without lost data and save the result in a file.
+vcftools --vcf ../data/wolf_2020_JG.vcf --freq --max-missing 1 --out ../data/clear_data
+
+#e) How many sites have a minor allele frequency <0.05?
+vcftools --vcf ../data/wolf_2020_JG.vcf --max-maf 0.05
+
+#f) Calculate the heterozygosity of each individual.
+vcftools --vcf ../data/wolf_2020_JG.vcf --het --out ../data/heterozygosity
+
+#g) Calculate the nucleotide diversity per site.
+vcftools --vcf ../data/wolf_2020_JG.vcf --site-pi --out ../data/diversity
+
+#h) Calculate nucleotide diversity per site only for chromosome 3 sites
+vcftools --vcf ../data/wolf_2020_JG.vcf --chr chr03 --site-pi --out ../data/diversity_Chr03
+
+#i) Filter sites that have a minor allele frequency <0.05 and create a new file called wolves_maf05.vcf.
+vcftools --vcf ../data/wolf_2020_JG.vcf --max-maf 0.05 --recode --out ../data/wolves_maf05.vcf
+
+#j) Convert the wolves_maf05.vcf file to plink format.
+vcftools --vcf ../data/wolves_maf05.recode.vcf --plink
